b27259e206f2d628592dcf4e96dbffb87bfa405f
laiwf <laiwf@zendaimoney.com>
2018-07-12 11:54:17 +0800

    初始化

---
+++ src/main/java/com/zilanghuo/ProjectSummaryApplication.java
@@ -0,0 +1,12 @@
+package com.zilanghuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSummaryApplication.class, args);
+	}
+}
