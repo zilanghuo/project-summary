fadbaca12907d91e57678bf09bcd7066de4f86ab
laiwf <laiwf@zendaimoney.com>
2018-07-24 09:40:56 +0800

    增加项目路径

--- src/main/java/com/zilanghuo/controller/HomeController.java
+++ src/main/java/com/zilanghuo/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.zilanghuo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
  * use:
  */
 @Controller
-@RequestMapping("/index")
+@RequestMapping(value = "/")
+@Slf4j
 public class HomeController {
 
     @RequestMapping("/")
     public String home() {
+        log.info("add index jsp");
         return "index";
     }
 }
