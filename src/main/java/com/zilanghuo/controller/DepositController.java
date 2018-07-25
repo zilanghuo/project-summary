2bca40308ae8bf7dcf732dd638c33259007c5204
laiwf <laiwf@zendaimoney.com>
2018-07-25 10:19:54 +0800

    增加Thymeleaf 配置

--- src/main/java/com/zilanghuo/controller/DepositController.java
+++ src/main/java/com/zilanghuo/controller/DepositController.java
@@ -1,7 +1,5 @@
 package com.zilanghuo.controller;
 
-import cn.hutool.core.util.CharsetUtil;
-import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.asymmetric.AsymmetricAlgorithm;
 import cn.hutool.crypto.asymmetric.AsymmetricCrypto;
 import cn.hutool.crypto.asymmetric.KeyType;
@@ -27,7 +25,7 @@ import java.util.TreeMap;
 @Slf4j
 public class DepositController {
 
-    AsymmetricCrypto crypto = new AsymmetricCrypto(AsymmetricAlgorithm.RSA, "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAO2u+9vkuQGYwe2Yw0XFXRdhCkvwY4328H2STJjeW2LGDJqYYQVbpw1CNwJ0hKKcpk1/APENbdv84RP7x3YEkQVNoB0uSj8qnpUsnuyBdxLdToohikvOrNRWgQx/ZvgFE+rWjka9wVaKqLUbUWxpq9GiUAPFa78kYOABD8dIMtg9AgMBAAECgYEAgpNzQiaxjLMDNyiJfrcioUlqgrWZu9BB5nqNIh5mTilHm1bDVlI3wAz0c6DXjQ5KPqDbP5KFHCoc7QGRXsC7egNBX9kNtL7ZCuYw78pE5sNM4+885fgoqaBCbnc+PxgyAqQ+ZIO5u6QKXQpEoe7PpvxCVBAGyn/1klaQVidUivECQQD87PnV05v8ibOv0N6cSpEZ8s/mdFVDSw0sFBdxMseFGY/WjDl1g9ZQCuwjrcT5S/mnYgb6MzRJn+s0rfCFlImLAkEA8JKURMVg6GqIleQq4e03uqEZ6AgErBlh2e+1/T9vgij6n/ueZysamHydZAupk3Wsfn1bkmdA4zqOCf7UZueOVwJAHDwIF8qrmyF0IahbcW8Ri6gDdWJ/MifqrIUBqO1WQJF98SFuOKQjBIRzn/gCCSJmGD1lMgENUTq88wCH3SGbyQJBAJzEuDAUe3EZM0aSOEufvQg2QV6OExVfOP+/ENYmB3FHaQLmAjRyx1MFKb9vRiMctLp80DaYaJVqq/Lhh+JDFOMCQQCXrBhjTx4KfLzfUhOVzm5D8w5sAn9Sg1jDeMwe8tyiyUBbbkw+k9qK0YLOfnwKuC3MNI5URjaKyLzilPDZZkrs","MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDcp9NU29EfqPReJLGBS0WZwCKxORrc4IQpKbup1cF4KzQnpMCwcJXF9KW1vJ/ZzOMwAlGfhq2V96MGPOO6T/Zkesasjdmy19wnOdzDxGXu2pEMbFMDOonYxf1m5/VNs2+TZ18eyW585XefXoNlYCzg6RJmXK0fZ1UPAU9ZxgocEQIDAQAB");
+    AsymmetricCrypto crypto = new AsymmetricCrypto(AsymmetricAlgorithm.RSA, "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAO2u+9vkuQGYwe2Yw0XFXRdhCkvwY4328H2STJjeW2LGDJqYYQVbpw1CNwJ0hKKcpk1/APENbdv84RP7x3YEkQVNoB0uSj8qnpUsnuyBdxLdToohikvOrNRWgQx/ZvgFE+rWjka9wVaKqLUbUWxpq9GiUAPFa78kYOABD8dIMtg9AgMBAAECgYEAgpNzQiaxjLMDNyiJfrcioUlqgrWZu9BB5nqNIh5mTilHm1bDVlI3wAz0c6DXjQ5KPqDbP5KFHCoc7QGRXsC7egNBX9kNtL7ZCuYw78pE5sNM4+885fgoqaBCbnc+PxgyAqQ+ZIO5u6QKXQpEoe7PpvxCVBAGyn/1klaQVidUivECQQD87PnV05v8ibOv0N6cSpEZ8s/mdFVDSw0sFBdxMseFGY/WjDl1g9ZQCuwjrcT5S/mnYgb6MzRJn+s0rfCFlImLAkEA8JKURMVg6GqIleQq4e03uqEZ6AgErBlh2e+1/T9vgij6n/ueZysamHydZAupk3Wsfn1bkmdA4zqOCf7UZueOVwJAHDwIF8qrmyF0IahbcW8Ri6gDdWJ/MifqrIUBqO1WQJF98SFuOKQjBIRzn/gCCSJmGD1lMgENUTq88wCH3SGbyQJBAJzEuDAUe3EZM0aSOEufvQg2QV6OExVfOP+/ENYmB3FHaQLmAjRyx1MFKb9vRiMctLp80DaYaJVqq/Lhh+JDFOMCQQCXrBhjTx4KfLzfUhOVzm5D8w5sAn9Sg1jDeMwe8tyiyUBbbkw+k9qK0YLOfnwKuC3MNI5URjaKyLzilPDZZkrs", "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDcp9NU29EfqPReJLGBS0WZwCKxORrc4IQpKbup1cF4KzQnpMCwcJXF9KW1vJ/ZzOMwAlGfhq2V96MGPOO6T/Zkesasjdmy19wnOdzDxGXu2pEMbFMDOonYxf1m5/VNs2+TZ18eyW585XefXoNlYCzg6RJmXK0fZ1UPAU9ZxgocEQIDAQAB");
 
     @RequestMapping(value = "/pay")
     public String pay(HttpServletRequest request, ModelMap model) {
@@ -35,30 +33,30 @@ public class DepositController {
         treeMap.put("ver", "1.00");
         treeMap.put("code", "regUserByFive");
         treeMap.put("mchnt_cd", "0002900F0352200");
-        treeMap.put("mchnt_txn_ss", "201807240000002");
+        treeMap.put("mchnt_txn_ssn", "201807240000002");
         treeMap.put("client_tp", "0");
         treeMap.put("usr_attr", "1");
         treeMap.put("page_notify_url", "https://www.baidu.com");
         treeMap.put("back_notify_url", "https://www.baidu.com");
-        log.info("{}",treeMap);
+        log.info("{}", treeMap);
 
-        model.put("ver", "1.00");
-        model.put("code", "regUserByFive");
-        model.put("mchnt_cd", "0002900F0352200");
-        model.put("mchnt_txn_ss", System.currentTimeMillis() + "");
-        model.put("client_tp", "1");
-        model.put("usr_attr", "1");
-        model.put("page_notify_url", "https://www.baidu.com");
-        model.put("back_notify_url", "https://www.baidu.com");
+        model.put("ver", treeMap.get("ver"));
+        model.put("code", treeMap.get("code"));
+        model.put("mchnt_cd", treeMap.get("mchnt_cd"));
+        model.put("mchnt_txn_ssn", treeMap.get("mchnt_txn_ssn"));
+        model.put("client_tp", treeMap.get("client_tp"));
+        model.put("usr_attr", treeMap.get("usr_attr"));
+        model.put("page_notify_url", treeMap.get("page_notify_url"));
+        model.put("back_notify_url", treeMap.get("back_notify_url"));
         Map<String, String> sign = sign(treeMap);
         System.out.println(sign);
-
-        return "deposit/pay";
+        model.put("signature", sign.get("signature"));
+        return "/deposit/pay";
     }
 
 
-    public Map<String,String> sign(TreeMap<String, String> treeMap) {
-        Map<String,String> result = new HashMap();
+    public Map<String, String> sign(TreeMap<String, String> treeMap) {
+        Map<String, String> result = new HashMap();
         Iterator<Map.Entry<String, String>> iterator = treeMap.entrySet().iterator();
         StringBuffer sb = new StringBuffer();
         while (iterator.hasNext()) {
@@ -68,9 +66,9 @@ public class DepositController {
             }
         }
         sb.deleteCharAt(sb.length() - 1);
-        result.put("key",sb.toString());
+        result.put("key", sb.toString());
         String pre = crypto.encryptBcd(sb.toString(), KeyType.PrivateKey);
-        result.put("signature",pre);
+        result.put("signature", pre);
 
         return result;
     }
